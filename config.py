b47df21d1a6f670a1537e90a01f12099f00bb37b
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-05-13 20:53:10 +0530

    Add STUN and TURN server configuration options

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "")
 COOKIES_PATH = os.getenv("COOKIES_PATH", "")
+
+STUN_SERVER    = os.getenv("STUN_SERVER",    "stun:stun.l.google.com:19302")
+TURN_SERVER    = os.getenv("TURN_SERVER",    "")
+TURN_USERNAME  = os.getenv("TURN_USERNAME",  "")
+TURN_PASSWORD  = os.getenv("TURN_PASSWORD",  "")
