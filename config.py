3bea6e49614a78ccae4330e2345c7458474abed5
Sinchu-XD <abhiyanshicreation@gmail.com>
2026-05-13 13:37:54 +0530

    Refactor environment variable assignments in config.py

--- config.py
+++ config.py
@@ -3,12 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID            = int(os.getenv("API_ID", "0"))
-API_HASH          = os.getenv("API_HASH", "")
-BOT_TOKEN         = os.getenv("BOT_TOKEN", "")
-ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "assistant")
-COOKIES_PATH      = os.getenv("COOKIES_PATH", "")   # Optional: YouTube cookies file path
-API_ID = int(os.getenv("API_ID", "35362137"))
-API_HASH = os.getenv("API_HASH", "c3c3e167ea09bc85369ca2fa3c1be790")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8360461005:AAH7uHgra-bYu1I3WOSgpn1VMrFt1Wi1fcw")
-ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "BQIblVkAH2XJrDMiKPGUoIfk4Pr5x0rGPb-mqrs6J4IPMZpSR-QJ6Df3u3iAIhaoZPDWDAfVBaC0PX6GlGdLGobcty7lskWfLDa9PplwsBvPlCFkPaJNB2U6EgdJZptzxrLidISSyqWLVawbpBRZvXWnNZn-qIvOXx-EX1p2koYiCvAOYq3NTSkleVvpnv1Q0Tuo7G2JdU5D7ayhuPXz6tjc6DAIracshRb2vh_KXmFar4mPxXP_VEDsTbtp4bioIsE1Dkh106Uw7PAFTM2LmKTBdLn4ys6OuqH5GSa-TTWgCl_k3G2ScrLOEaovYXvRkAGg3IDnGe23_WOfDV2cvbngcuPRvgAAAAFtutilAA" )
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "")
+COOKIES_PATH = os.getenv("COOKIES_PATH", "")
